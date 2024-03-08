e13cea5e929ff1a7e34f8536ba91e23c89d5643e
g-jensen <greg.micah.jensen@gmail.com>
2024-03-08 18:11:42 -0500

    updated string specs

--- spec/c/string_spec.c
+++ spec/c/string_spec.c
@@ -5,42 +5,42 @@ module(string, {
     describe("string_charAt",
 
         it("gets first index",
-            should(string_charAt("hello",0) == 'h'))
+            should_eq('h',string_charAt("hello",0),char))
         
         it("gets second index",
-            should(string_charAt("bye",1) == 'y')));
+            should_eq('y',string_charAt("bye",1),char)));
 
     describe("string_length",
     
         it("zero length",
-            should(string_length("") == 0))
+            should_eq(0,string_length(""),int))
             
         it("length 1",
-            should(string_length("a") == 1))
+            should_eq(1,string_length("a"),int))
         
         it("length 3",
-            should(string_length("abc") == 3)));
+            should_eq(3,string_length("abc"),int)));
 
     describe("string_compareTo",
                     
         it("compares to empty string",
-            should(string_compareTo("","") == 0);
-            should(string_compareTo("a","") == 1)
-            should(string_compareTo("","a") == -1))
+            should_eq(0,string_compareTo("",""),int);
+            should_eq(1,string_compareTo("a",""),int)
+            should_eq(-1,string_compareTo("","a"),int))
         
         it("compares with first char different",
-            should(string_compareTo("z","a") == 25)
-            should(string_compareTo("xbc","ae") == 23))
+            should_eq(25,string_compareTo("z","a"),int)
+            should_eq(23,string_compareTo("xbc","ae"),int))
             
         it("compares with second char different",
-            should(string_compareTo("ab","ac") == -1))
+            should_eq(-1,string_compareTo("ab","ac"),int))
             
         it("compares with third char different",
-            should(string_compareTo("abi","abf") == 3))
+            should_eq(3,string_compareTo("abi","abf"),int))
             
         it("compares exact same strings",
-            should(string_compareTo("hello there","hello there") == 0)
-            should(string_compareTo("bye bye","bye bye") == 0)));
+            should_eq(0,string_compareTo("hello there","hello there"),int)
+            should_eq(0,string_compareTo("bye bye","bye bye"),int)));
 
     describe("string_equals",
         
@@ -63,57 +63,57 @@ module(string, {
         
         it("copies empty string",
             char* out = string_copy("");
-            should(string_equals(out,""));
+            should_str_eq("",out);
             free(out);)
             
         it("copies populated string",
             char* out = string_copy("h");
-            should(string_equals(out,"h"));
+            should_str_eq("h",out);
             free(out);
             
             out = string_copy("hi");
-            should(string_equals(out,"hi"));
+            should_str_eq("hi",out);
             free(out);
             
             out = string_copy("bye");
-            should(string_equals(out,"bye"));
+            should_str_eq("bye",out);
             free(out);));
 
     describe("string_toLowerCase",
         
         it("does nothing to lowercase string",
             char* out = string_toLowerCase("");
-            should(string_equals(out,""));
+            should_str_eq("",out);
             free(out);
             
             out = string_toLowerCase("a");
-            should(string_equals(out,"a"));
+            should_str_eq("a",out);
             free(out);)
             
         it("converts uppcase to lowercase",
             char* out = string_toLowerCase("A");
-            should(string_equals(out,"a"));
+            should_str_eq("a",out);
             free(out);
             
             out = string_toLowerCase("Ab");
-            should(string_equals(out,"ab"));
+            should_str_eq("ab",out);
             free(out);
             
             out = string_toLowerCase("AB");
-            should(string_equals(out,"ab"));
+            should_str_eq("ab",out);
             free(out);));
 
     describe("string_compareToIgnoreCase",
         
         it("has normal compareTo behavior for strings of same case",
-            should(string_compareToIgnoreCase("","") == 0);
-            should(string_compareToIgnoreCase("a","") == 1);
-            should(string_compareToIgnoreCase("","a") == -1);
-            should(string_compareToIgnoreCase("abi","abf") == 3);
-            should(string_compareToIgnoreCase("ABI","ABF") == 3);)
+            should_eq(0,string_compareToIgnoreCase("",""),int);
+            should_eq(1,string_compareToIgnoreCase("a",""),int);
+            should_eq(-1,string_compareToIgnoreCase("","a"),int);
+            should_eq(3,string_compareToIgnoreCase("abi","abf"),int);
+            should_eq(3,string_compareToIgnoreCase("ABI","ABF"),int);)
             
         it("ignores case",
-            should(string_compareToIgnoreCase("abi","Abf") == 3);
-            should(string_compareToIgnoreCase("abi","ABF") == 3);
-            should(string_compareToIgnoreCase("bye BYE","BYE bye") == 0);));
+            should_eq(3,string_compareToIgnoreCase("abi","Abf"),int);
+            should_eq(3,string_compareToIgnoreCase("abi","ABF"),int);
+            should_eq(0,string_compareToIgnoreCase("bye BYE","BYE bye"),int);));
 });

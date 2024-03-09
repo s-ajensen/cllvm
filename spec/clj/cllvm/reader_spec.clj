29a19614c3b45a7fde5dfda609b58e962675e2fa
g-jensen <greg.micah.jensen@gmail.com>
2024-03-08 21:43:28 -0500

    implement reading strings

--- spec/clj/cllvm/reader_spec.clj
+++ spec/clj/cllvm/reader_spec.clj
@@ -3,14 +3,17 @@
             [speclj.core :refer :all]
             [cllvm.reader :as sut]))
 
-;; TODO - [GMJ] let me know what you think about this
 (defn with-boilerplate [& lines]
   (apply util/lines->str
          (concat ["; ModuleID = 'test'"
                   "source_filename = \"test.ll\""
                   "%TypeTag = type { i32 }"
-                  "%Primitive = type { %TypeTag, ptr }"
-                  "define %Primitive* @eval() {"
+                  "%Primitive = type { %TypeTag, ptr }"]
+                 lines)))
+
+(defn in-eval [& lines]
+  (apply util/lines->str
+         (concat ["define %Primitive* @eval() {"
                   "entry:"]
                  lines
                  ["}"])))
@@ -22,19 +25,19 @@
 
     (it "defines default module and filename"
       (should-start-with (str
-                 "; ModuleID = 'user'\n"
-                 "source_filename = \"user.ll\"\n")
+                          "; ModuleID = 'user'\n"
+                          "source_filename = \"user.ll\"\n")
         (sut/ast->ir [:exp])))
 
     (it "defines specific module and filename"
       (should-start-with (str
-                 "; ModuleID = 'test'\n"
-                 "source_filename = \"test.ll\"\n")
+                          "; ModuleID = 'test'\n"
+                          "source_filename = \"test.ll\"\n")
         (sut/ast->ir "test" [:exp])))
 
     (it "defines main entry point"
       (should= (with-boilerplate
-                 "ret %Primitive* null")
+                 (in-eval "ret %Primitive* null"))
         (sut/ast->ir "test" [:exp]))))
 
   (context "expressions"
@@ -44,25 +47,42 @@
       (context "number"
 
         (it "long"
-          (should= (with-boilerplate
-                     "%ptr_0 = alloca %Primitive, align 8"
-                     "%ptr_1 = getelementptr %Primitive, %Primitive* %ptr_0, i32 0, i32 0"
-                     "store i32 0, i32* %ptr_1"
-                     "%ptr_2 = getelementptr %Primitive, %Primitive* %ptr_0, i32 0, i32 1"
-                     "%val_3 = alloca i64, align 8"
-                     "store i64 123, i64* %val_3, align 8"
-                     "store i64* %val_3, ptr %ptr_2, align 8"
-                     "ret %Primitive* %ptr_0")
+          (should= (with-boilerplate 
+                     (in-eval 
+                      "%ptr_0 = alloca %Primitive, align 8" 
+                      "%ptr_1 = getelementptr %Primitive, %Primitive* %ptr_0, i32 0, i32 0" 
+                      "store i32 0, i32* %ptr_1" 
+                      "%ptr_2 = getelementptr %Primitive, %Primitive* %ptr_0, i32 0, i32 1" 
+                      "%val_3 = alloca i64, align 8" 
+                      "store i64 123, i64* %val_3, align 8" 
+                      "store i64* %val_3, ptr %ptr_2, align 8" 
+                      "ret %Primitive* %ptr_0"))
             (sut/ast->ir "test" [:exp [:lit [:num [:long "123"]]]])))
 
         (it "double"
           (should= (with-boilerplate
-                     "%ptr_0 = alloca %Primitive, align 8"
-                     "%ptr_1 = getelementptr %Primitive, %Primitive* %ptr_0, i32 0, i32 0"
-                     "store i32 1, i32* %ptr_1"
-                     "%ptr_2 = getelementptr %Primitive, %Primitive* %ptr_0, i32 0, i32 1"
-                     "%val_3 = alloca double, align 8"
-                     "store double 1.23, double* %val_3, align 8"
-                     "store double* %val_3, ptr %ptr_2, align 8"
-                     "ret %Primitive* %ptr_0")
-            (sut/ast->ir "test" [:exp [:lit [:num [:double "1.23"]]]])))))))
+                     (in-eval 
+                      "%ptr_0 = alloca %Primitive, align 8" 
+                      "%ptr_1 = getelementptr %Primitive, %Primitive* %ptr_0, i32 0, i32 0" 
+                      "store i32 1, i32* %ptr_1" 
+                      "%ptr_2 = getelementptr %Primitive, %Primitive* %ptr_0, i32 0, i32 1" 
+                      "%val_3 = alloca double, align 8" 
+                      "store double 1.23, double* %val_3, align 8" 
+                      "store double* %val_3, ptr %ptr_2, align 8" 
+                      "ret %Primitive* %ptr_0"))
+            (sut/ast->ir "test" [:exp [:lit [:num [:double "1.23"]]]]))))
+            
+      (it "string"
+        
+        (should= (with-boilerplate 
+                   (in-eval
+                    "%str_0 = alloca [5 x i8], align 1"
+                    "store [5 x i8] c\"test\\00\", [5 x i8]* %str_0, align 1"
+                    "%ptr_0 = alloca %Primitive, align 8"
+                    "%ptr_1 = getelementptr %Primitive, %Primitive* %ptr_0, i32 0, i32 0"
+                    "store i32 2, i32* %ptr_1"
+                    "%ptr_2 = getelementptr %Primitive, %Primitive* %ptr_0, i32 0, i32 1"
+                    "%ptr_3 = getelementptr [5 x i8], [5 x i8]* %str_0, i64 0, i64 0"
+                    "store i8* %ptr_3, ptr %ptr_2, align 8"
+                    "ret %Primitive* %ptr_0"))
+                 (sut/ast->ir "test" [:exp [:lit [:str "\"test\""]]]))))))
--- src/clj/cllvm/reader.clj
+++ src/clj/cllvm/reader.clj
@@ -1,6 +1,8 @@
 (ns cllvm.reader
-  (:require [cllvm.ll.core :as ll :refer [->* _ptr _i32 _i64 _double type-codes]]
-            [cllvm.util :as util]))
+  (:require [cllvm.ll.core :as ll :refer [->* _double _i32 _i64 _ptr
+                                          type-codes]]
+            [cllvm.util :as util]
+            [clojure.string :as str]))
 
 (def var-idx (atom 0))
 (defn var-idx! []
@@ -13,6 +15,15 @@
 
 (defn ->val-sym! []
   (str "%val_" (var-idx!)))
+
+(def str-idx (atom 0))
+(defn str-idx! []
+  (let [idx @var-idx]
+    (swap! str-idx inc)
+    idx))
+
+(defn ->str-sym! []
+  (str "%str_" (str-idx!)))
 
 (def prim "%Primitive")
 (def prim* (str prim "*"))
@@ -42,6 +53,25 @@
       (ll/store address*-sym (->* type) _ptr value*-sym 8)
       (ll/ret prim* prim*-sym))))
 
+(defn ret-str [val]
+  (let [evaluated-str (read-string val)
+        str-size (inc (count evaluated-str))
+        str-type (str "[" str-size " x i8]")
+        str*-sym (->str-sym!)
+        prim*-sym (->ptr-sym!)
+        type*-sym (->ptr-sym!)
+        address*-sym  (->ptr-sym!)
+        str-ptr*-sym (->ptr-sym!)]
+    (util/lines->str 
+      (ll/def-str str*-sym evaluated-str)
+      (ll/alloca prim*-sym prim 8) 
+      (ll/get-element* type*-sym prim prim*-sym [_i32 0] [_i32 0]) 
+      (ll/store type*-sym _i32 (type-codes "str")) 
+      (ll/get-element* address*-sym prim prim*-sym [_i32 0] [_i32 1])
+      (ll/get-element* str-ptr*-sym str-type str*-sym [_i64 0] [_i64 0])
+      (ll/store address*-sym "i8*" _ptr str-ptr*-sym 8)
+      (ll/ret prim* prim*-sym))))
+
 (defmethod expr->ir :num [[_ body]]
   (expr->ir body))
 (defmethod expr->ir :long [[_ body]]
@@ -49,6 +79,9 @@
 
 (defmethod expr->ir :double [[_ body]]
   (ret-prim _double body))
+
+(defmethod expr->ir :str [[_ body]]
+  (ret-str body))
 
 (defn ast->ir
   ([expr] (ast->ir "user" expr))
